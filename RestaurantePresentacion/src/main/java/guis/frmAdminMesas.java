386e54c7bf2c95a5d2fb4dd0cf0e55c65fbf823a
Gh0zTy1 <160164643+Gh0zTy1@users.noreply.github.com>
2024-10-27 17:08:30 -0700

    listo. cree el main

--- RestaurantePresentacion/src/main/java/guis/frmAdminMesas.java
+++ RestaurantePresentacion/src/main/java/guis/frmAdminMesas.java
@@ -28,6 +28,7 @@ public class frmAdminMesas extends javax.swing.JFrame {
      * Creates new form frmAdminMesas
      */
     public frmAdminMesas() {
+        this.setLocationRelativeTo(null);
         initComponents();
         this.setTitle("Administrar Mesas");
         //btnRegistrarMesas.addActionListener(this::btnRegistrarMesasActionPerformed);
@@ -278,41 +279,7 @@ cbxUbicacionMesa.setModel(new javax.swing.DefaultComboBoxModel<>(UbicacionMesaDT
     nuevoFrame.setVisible(true);
     }//GEN-LAST:event_CancelarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmAdminMesas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmAdminMesas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmAdminMesas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmAdminMesas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new frmAdminMesas().setVisible(true);
-            }
-        });
-    }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Cancelar;
     private javax.swing.JButton btnRegistrarMesas;
