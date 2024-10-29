69f9b604c870fb5ff7c8a376fe9a50f5bc6c2196
Gh0zTy1 <160164643+Gh0zTy1@users.noreply.github.com>
2024-10-29 00:15:00 -0700

    Adaptacion de la entidad "Restarante" a la capa de persistencia

--- RestaurantePersistencia/src/main/java/dao/implementaciones/RestaurantesDAO.java
+++ RestaurantePersistencia/src/main/java/dao/implementaciones/RestaurantesDAO.java
@@ -5,10 +5,14 @@
 
 package dao.implementaciones;
 
+import conexion.Conexion;
 import dao.interfaces.IRestaurantesDAO;
 import entidades.Restaurante;
 import excepciones.DAOException;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 
 /**
  * Implementa los metodos definidos por IRestaurantesDAO
@@ -30,20 +34,42 @@ public class RestaurantesDAO implements IRestaurantesDAO {
     
     @Override
     public List<Restaurante> obtenerRestaurantesTodos() throws DAOException {
-        // TODO: IMPLEMENTAR ESTE
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManager entityManager = Conexion.getInstance().crearConexion();
+        try {
+            TypedQuery<Restaurante> query = entityManager.createQuery("SELECT r FROM Restaurante r", Restaurante.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DAOException("Error al obtener todos los restaurantes");
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Restaurante obtenerRestaurantePorID(Long id) throws DAOException {
-        // TODO: IMPLEMENTAR ESTE
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManager entityManager = Conexion.getInstance().crearConexion();
+        try {
+            return entityManager.find(Restaurante.class, id);
+        } catch (Exception e) {
+            throw new DAOException("Error al obtener el restaurante por ID");
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Restaurante obtenerRestaurantePorNumeroTelefono(String numeroTelefono) throws DAOException {
-        // TODO: IMPLEMENTAR ESTE
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManager entityManager = Conexion.getInstance().crearConexion();
+        try {
+            TypedQuery<Restaurante> query = entityManager.createQuery(
+                "SELECT r FROM Restaurante r WHERE r.telefono = :telefono", Restaurante.class);
+            query.setParameter("telefono", numeroTelefono);
+            return query.getSingleResult();
+        } catch (Exception e) {
+            throw new DAOException("Error al obtener el restaurante por número de teléfono");
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
